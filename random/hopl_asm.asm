27fa4871525ee885354eacff4a0c9c771431576f
CowerDawn <bigdickpopboba@gmail.com>
2025-03-09 11:46:55 +0300

    Update hopl_asm.asm

--- random/hopl_asm.asm
+++ random/hopl_asm.asm
@@ -15,3 +15,39 @@ _start:
     mov eax, 60             
     xor edi, edi            
     syscall
+
+section .data
+    message db 'HOPL', 0xA  
+    len equ $ - message      
+
+section .text
+    global _start
+
+_start:
+    mov eax, 1              
+    mov edi, 1              
+    lea rsi, [message]      
+    mov edx, len            
+    syscall
+
+    mov eax, 60             
+    xor edi, edi            
+    syscall
+
+section .data
+    message db 'HOPL', 0xA  
+    len equ $ - message      
+
+section .text
+    global _start
+
+_start:
+    mov eax, 1              
+    mov edi, 1              
+    lea rsi, [message]      
+    mov edx, len            
+    syscall
+
+    mov eax, 60             
+    xor edi, edi            
+    syscall
